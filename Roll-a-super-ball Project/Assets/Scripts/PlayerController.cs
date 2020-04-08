1704cd5d4f89593c8d103c51b117a36a222fd269
mauu95 <mauu95@hotmail.it>
2020-04-08 21:36:02 +0200

    The starting point is set: End of Roll a ball Tutorial
    Oh sono andato un pò a memoria... forse non è uguale uguale

---
+++ Roll-a-super-ball Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public int speed;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "COUNT: " + count.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "COUNT: " + count.ToString();
+
+            if (count == 4)
+                winText.gameObject.SetActive(true);
+        }
+            
+    }
+
+}
