55d59d00cf73aa7faa361ac0f07e91455177bfda
mauu95 <mauu95@hotmail.it>
2020-04-09 12:01:51 +0200

    Camera rotation with mouse

--- Roll-a-super-ball_Project/Assets/Scripts/CameraController.cs
+++ Roll-a-super-ball_Project/Assets/Scripts/CameraController.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
 
-    private void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+    public float MouseHorizontalSpeed = 2.0f;
+    public float MouseVerticalSpeed = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
     private void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position;
+
+        yaw += MouseHorizontalSpeed * Input.GetAxis("Mouse X");
+        pitch -= MouseVerticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        print(Vector3.forward);
     }
 }
