1e33a1a13cadb7478c1740eda07ca57cfaf798e4
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-09 22:21:33 +0200

    Added a Jumper

---
+++ Roll-a-super-ball_Project/Assets/Scripts/PulseForceEmitter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseForceEmitter : MonoBehaviour {
+    public float force = 5f;
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("hello world 1");
+        if (collision.gameObject.CompareTag("Player")) {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            if (rb) {
+                rb.AddForce(0f, force, 0f, ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("hello world 2");
+        if (other.gameObject.CompareTag("Player")) {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb) {
+                rb.AddForce(0f, force, 0f, ForceMode.Impulse);
+            }
+        }
+    }
+}
