ec415176e74e8036024fd69414f1b2efa2102c1a
mauu95 <mauu95@hotmail.it>
2020-04-09 16:31:25 +0200

    Control when in air less reactive

--- Roll-a-super-ball_Project/Assets/Scripts/PlayerController.cs
+++ Roll-a-super-ball_Project/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float speedInAir;
     public Text countText;
     public Text winText;
     public Material bounceMaterial;
     public Transform MainCamera;
+    public float JumpForce;
 
     private Rigidbody rb;
     private int count;
@@ -29,20 +31,20 @@ public class PlayerController : MonoBehaviour
         if (canJump && isGrounded && Input.GetKey(KeyCode.Space))
         {
             isGrounded = false;
-            rb.AddForce(0f, 50f, 0f, ForceMode.Impulse);
+            rb.AddForce(0f, JumpForce, 0f, ForceMode.Impulse);
         }
 
-        Vector3 forward = Vector3.Normalize(MainCamera.position - transform.position) * -10;
-        Vector3 lateral = Vector3.Cross(forward, Vector3.up).normalized * -10;
+            Vector3 forward = Vector3.Normalize(MainCamera.position - transform.position) * -10;
+            Vector3 lateral = Vector3.Cross(forward, Vector3.up).normalized * -10;
+            Vector3 moveHorizontal = Input.GetAxis("Horizontal") * lateral;
+            Vector3 moveVertical = Input.GetAxis("Vertical") * forward;
+            Vector3 temp = moveHorizontal + moveVertical;
 
-        Vector3 moveHorizontal = Input.GetAxis("Horizontal") * lateral;
-        Vector3 moveVertical = Input.GetAxis("Vertical") * forward;
-
-        Vector3 temp = moveHorizontal + moveVertical;
-
-        Vector3 movement = new Vector3(temp.x, 0f, temp.z);
-
-        rb.AddForce(movement * speed, ForceMode.VelocityChange);
+            Vector3 movement = new Vector3(temp.x, 0f, temp.z);
+            if(isGrounded)
+                rb.AddForce(movement * speed, ForceMode.VelocityChange);
+            else
+                rb.AddForce(movement * speedInAir , ForceMode.VelocityChange);
     }
 
     private void OnTriggerEnter(Collider other)
