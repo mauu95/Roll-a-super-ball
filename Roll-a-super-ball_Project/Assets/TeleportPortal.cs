330893c427294d5e114185f8ca58d1f88a94a900
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-10 22:45:40 +0200

    Added portals

---
+++ Roll-a-super-ball_Project/Assets/TeleportPortal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPortal : MonoBehaviour {
+    public TeleportPortal otherPortal;
+    private bool playerIsComing = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player") && !playerIsComing) {
+            if (!playerIsComing) {
+                otherPortal.TeleportingHere();
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                rb.velocity = otherPortal.gameObject.transform.forward * rb.velocity.magnitude;
+                other.gameObject.transform.position = otherPortal.gameObject.transform.position;
+            } else {
+                playerIsComing = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        playerIsComing &= !other.gameObject.CompareTag("Player");
+    }
+
+    public void TeleportingHere() {
+        playerIsComing = true;
+    }
+}
