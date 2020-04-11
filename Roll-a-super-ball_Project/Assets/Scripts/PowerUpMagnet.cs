9365e4d1ec82c4c97a80660bf15daf04470c1dd0
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-11 17:18:44 +0200

    Magnet powerup

---
+++ Roll-a-super-ball_Project/Assets/Scripts/PowerUpMagnet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpMagnet : PowerUp {
+    public float speed = 1;
+    public float range = 10;
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, transform.forward);
+            AttractHitObjects(hits);
+        }
+    }
+
+    private void AttractHitObjects(RaycastHit[] hits) {
+        foreach (RaycastHit hit in hits) {
+            if (hit.transform.CompareTag("PickUp")) {
+                Vector3 distance = transform.position - hit.transform.position;
+                hit.transform.position += distance.normalized * speed * Time.deltaTime;
+            }
+        }
+
+    }
+
+}
