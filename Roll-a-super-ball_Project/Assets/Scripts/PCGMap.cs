30b2fd514e325d3193f0bd848b8a40075c6e7275
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-13 22:58:17 +0200

    PCGMap adds portals

--- Roll-a-super-ball_Project/Assets/Scripts/PCGMap.cs
+++ Roll-a-super-ball_Project/Assets/Scripts/PCGMap.cs
@@ -4,39 +4,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PCGMap : MonoBehaviour
-{
+public class PCGMap : MonoBehaviour {
     public int seed;
     public GameObject platformPrefab;
     public GameObject BrigdePrefab;
     public GameObject carloPrefab;
+    public GameObject portalPrefab;
     public int Dimension = 20;
     public int nFloor = 1;
     public int nCarli;
 
     private IteratorSeed iseed;
     private List<Vector3> platforms;
+    private List<TeleportPortal> portals;
     private GameObject map;
 
     private GameObject currentFloor;
 
-    private void Start()
-    {
+    private void Start() {
         CreateMap();
     }
 
-    private void CreateMap()
-    {
+    private void CreateMap() {
         iseed = new IteratorSeed(seed);
         platforms = new List<Vector3>();
+        portals = new List<TeleportPortal>();
 
         GameObject temp = new GameObject();
         map = Instantiate(temp);
         Destroy(temp);
         map.name = "Map";
-
-        for (int i = 0; i < nFloor; i++)
-        {
+        List<int> platformIndexes = new List<int>();
+        for (int i = 0; i < nFloor; i++) {
             temp = new GameObject();
             currentFloor = Instantiate(temp, map.transform);
             Destroy(temp);
@@ -44,11 +43,25 @@ public class PCGMap : MonoBehaviour
 
             transform.position = new Vector3(0f, 10f * i, 0f);
             CreateFloor();
+            platformIndexes.Add(platforms.Count - 1);
+        }
+
+        // ignore last index
+        for (int i = 0; i < platformIndexes.Count - 1; i++) {
+            int index = platformIndexes[i];
+
+            GameObject portalObject1 = Instantiate(portalPrefab, platforms[index] + Vector3.up, Quaternion.identity);
+            TeleportPortal portal1 = portalObject1.GetComponent<TeleportPortal>();
+
+            GameObject portalObject2 = Instantiate(portalPrefab, platforms[index + 1] + Vector3.up, Quaternion.identity);
+            TeleportPortal portal2 = portalObject2.GetComponent<TeleportPortal>();
+
+            portal1.otherPortal = portal2;
+            portal2.otherPortal = portal1;
         }
 
         //Place Carli
-        for(int i = 0; i < nCarli; i++)
-        {
+        for (int i = 0; i < nCarli; i++) {
             Vector3 platPos = platforms[iseed.Next(platforms.Count)];
             Vector3 carloPos = new Vector3(platPos.x + iseed.Next(10) - 4, platPos.y + 1, platPos.z + iseed.Next(10) - 4);
             Create(carloPrefab, carloPos, Quaternion.identity);
@@ -56,32 +69,24 @@ public class PCGMap : MonoBehaviour
 
     }
 
-    private void CreateFloor()
-    {
+    private void CreateFloor() {
 
         for (int i = 0; i < Dimension; i++)
             PerformAction();
     }
 
-    private void PerformAction()
-    {
+    private void PerformAction() {
         int action = iseed.Next(3);
 
-        if (action == 0)
-        {
-            if (!platforms.Contains(transform.position))
-            {
+        if (action == 0) {
+            if (!platforms.Contains(transform.position)) {
                 platforms.Add(transform.position);
                 Create(platformPrefab, transform.position, transform.rotation);
             }
-        }
-        else if (action == 1)
-        {
+        } else if (action == 1) {
             int newdirection = iseed.Next(4);
             transform.Rotate(0f, newdirection * 90f, 0f);
-        }
-        else if (action == 2)
-        {
+        } else if (action == 2) {
             Vector3 prev = transform.position;
             int distance = 5 + iseed.Next(2) * 10 + iseed.Next(9);
 
@@ -97,8 +102,7 @@ public class PCGMap : MonoBehaviour
         }
     }
 
-    private GameObject Create(GameObject obj, Vector3 pos, Quaternion rot)
-    {
+    private GameObject Create(GameObject obj, Vector3 pos, Quaternion rot) {
         return Instantiate(obj, pos, rot, currentFloor.transform);
     }
 
