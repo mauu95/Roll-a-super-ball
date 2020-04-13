3054059efee0363312cd8296f9d2ae22c0dc0e04
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-13 23:49:31 +0200

    Added Oval Player Malus

---
+++ Roll-a-super-ball_Project/Assets/Scripts/OvalPlayerMalus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvalPlayerMalus : MonoBehaviour {
+    // Start is called before the first frame update
+    public Vector3 size = new Vector3(1, 1.5f, 0.5f);
+    public float duration = 20;
+
+    private float elapsedTime = 0f;
+    private Vector3 initialScale;
+    void Start() {
+        initialScale = transform.localScale;
+        transform.localScale = new Vector3(1, 1.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= duration) {
+            transform.localScale = initialScale;
+            Destroy(this);
+        }
+
+    }
+}
---
+++ Roll-a-super-ball_Project/Assets/Scripts/PickableMalus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableMalus : MonoBehaviour {
+
+    public string type;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            if (type == "ovalPlayer") {
+                other.gameObject.AddComponent<OvalPlayerMalus>();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
