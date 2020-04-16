b3e191800f7dbc6f882fa58be0688a37b51c43a4
Taranjit Singh <taranjit.singh@spindox.it>
2020-04-16 23:00:55 +0200

    Magnet has an active time

--- Roll-a-super-ball_Project/Assets/Scripts/PowerUpMagnet.cs
+++ Roll-a-super-ball_Project/Assets/Scripts/PowerUpMagnet.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpMagnet : PowerUp {
+    public float activeTime = 5;
     public float speed = 10;
     public float range = 10;
 
-    
+    private float elapsedTime = 0;
+
+    private void Start() {
+        elapsedTime = activeTime;
+    }
 
     private void Update() {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (elapsedTime < activeTime) {
             RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, transform.forward);
             AttractHitObjects(hits);
+            elapsedTime += Time.deltaTime;
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            elapsedTime = 0;
         }
     }
 
@@ -25,8 +33,7 @@ public class PowerUpMagnet : PowerUp {
 
     }
 
-    public override string GetName()
-    {
+    public override string GetName() {
         return "magnet";
     }
 }
