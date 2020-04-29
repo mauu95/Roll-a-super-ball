57536aebdd6d04e766cea84a5fe170662ab8d071
mauu95 <mauu95@hotmail.it>
2020-04-29 19:00:54 +0200

    Added smaterializator effect

---
+++ Roll-a-super-ball_Project/Assets/_Scripts/Smaterializator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smaterializator : MonoBehaviour
+{
+    [Range(0,1)]
+    public float visibility;
+    public float fadeTime = 1f;
+
+    private Material mat;
+    private string visibilityName = "Vector1_6566B52F";
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        visibility = 1;
+    }
+    void Update()
+    {
+        float interp = (Time.deltaTime) / fadeTime * 2;
+        float vis = Mathf.Lerp( mat.GetFloat(visibilityName) , visibility, interp);
+        mat.SetFloat(visibilityName, vis);
+    }
+
+    public void FadeIn()
+    {
+        visibility = 1;
+    }
+
+    public void FadeOut()
+    {
+        visibility = 0;
+    }
+}
