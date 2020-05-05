89f85d0f350336e4de92f5d8fd970c089bebe719
mauu95 <mauu95@hotmail.it>
2020-05-05 21:59:09 +0200

    Modified PauseMenu

---
+++ Roll-a-super-ball_Project/Assets/MenuCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCreator : MonoBehaviour
+{
+    public GameObject menuPrefab;
+    void Start()
+    {
+        Instantiate(menuPrefab, transform.parent);
+        Destroy(gameObject);
+    }
+}
