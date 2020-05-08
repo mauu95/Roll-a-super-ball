2cd11bf09ed9126019404c43656cbc9bd47f75a5
mauu95 <mauu95@hotmail.it>
2020-05-08 23:05:48 +0200

    Save last settings/Other UI disabled

--- Roll-a-super-ball_Project/Assets/_Scripts/DifficultySetter.cs
+++ Roll-a-super-ball_Project/Assets/_Scripts/DifficultySetter.cs
@@ -7,30 +7,56 @@ using TMPro;
 public class DifficultySetter : MonoBehaviour
 {
     public PCGMap mapCreator;
+    public GameObject inventoryUI;
+    public GameObject scoreUI;
     public GameObject player;
 
     public Slider sizeSlider;
     public Slider nFloorSlider;
     public Slider nEnemySlider;
 
+    private int sliderSizeInit;
+    private int sliderNFloorInit;
+    private int sliderNEnemyInit;
+
     private void Start()
     {
         GameManager.instance.CursorOn();
+
+        sliderSizeInit = PlayerPrefs.GetInt("sliderSizeInit");
+        sliderNFloorInit = PlayerPrefs.GetInt("sliderNFloorInit");
+        sliderNEnemyInit = PlayerPrefs.GetInt("sliderNEnemyInit");
+
+        if (sliderSizeInit == 0)
+            sliderSizeInit = (int)sizeSlider.minValue;
+        if (sliderNFloorInit == 0)
+            sliderNFloorInit = (int)nFloorSlider.minValue;
+
+        sizeSlider.value = sliderSizeInit;
+        nFloorSlider.value = sliderNFloorInit;
+        nEnemySlider.value = sliderNEnemyInit;
     }
 
     public void StartLevel()
     {
         int dim = (int)sizeSlider.value;
         int nFloor = (int)nFloorSlider.value;
+        int nAgents = (int)nEnemySlider.value;
+
+        PlayerPrefs.SetInt("sliderSizeInit", dim);
+        PlayerPrefs.SetInt("sliderNFloorInit", nFloor);
+        PlayerPrefs.SetInt("sliderNEnemyInit", nAgents);
 
         mapCreator.Dimension = dim;
         mapCreator.nFloor = nFloor;
-        mapCreator.nAgents = (int)nEnemySlider.value;
+        mapCreator.nAgents = nAgents;
 
         mapCreator.nPickUps = (int)(dim * nFloor / 10);
 
         mapCreator.gameObject.SetActive(true);
         player.SetActive(true);
+        inventoryUI.SetActive(true);
+        scoreUI.SetActive(true);
 
         GameManager.instance.CursorOff();
 
