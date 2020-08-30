6bb0ec0e94111cfe4473e56c7bf2c83c17406abc
mauu95 <mauu95@hotmail.it>
2020-08-30 18:26:45 +0200

    Attracting pickups di defaut

---
+++ Roll-a-super-ball_Project/Assets/MiniMagnet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMagnet : MonoBehaviour
+{
+    public Transform follow;
+
+    private void Update()
+    {
+        if(follow != null)
+            transform.position = follow.position;
+    }
+
+    private void OnTriggerStay(Collider hit)
+    {
+        if (hit.transform.CompareTag("PickUp"))
+        {
+            Vector3 distance = transform.position - hit.transform.position;
+            hit.transform.position += distance.normalized * 8f * Time.deltaTime;
+        }
+    }
+}
---
+++ Roll-a-super-ball_Project/Assets/MiniMagnetCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMagnetCreator : MonoBehaviour
+{
+    public GameObject miniMagnet;
+
+    private void Start()
+    {
+        GameObject x = Instantiate(miniMagnet);
+        x.GetComponent<MiniMagnet>().follow = transform;
+    }
+}
