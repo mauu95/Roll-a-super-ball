98dc4ab9d5001d286fc76575c2b250306868d78c
mauu95 <mauu95@hotmail.it>
2020-09-01 16:47:35 +0200

    Added level title at the beginng

---
+++ Roll-a-super-ball_Project/Assets/LevelTitile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelTitile : MonoBehaviour
+{
+    void Start()
+    {
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+        int level = GameManager.instance.getCurrentLevel();
+
+
+        if (level >= GameManager.LEVEL_COUNT - 1)
+            text.text = "SPECIAL LEVEL";
+        else
+            text.text = "Level " + level;
+
+        Animator anim = GetComponent<Animator>();
+        if (anim)
+            anim.Play("TitleFadeOut");
+    }
+}
